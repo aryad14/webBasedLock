c8c1a794ee06361983a99497dd5d47ffe407d213
Arya Danech <aryadanech9@gmail.com>
2024-04-29 22:48:50 +0530

    [ADD] API for Adding Locks

--- server/api/userData/addLock.js
+++ server/api/userData/addLock.js
@@ -1,10 +1,10 @@
 import express from "express";
 import appFirebase from "../../db.js";
-import { getDatabase, ref, set } from "firebase/database";
 import { getAuth } from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const router = express.Router();
-const db = getDatabase(appFirebase);
+const db = getFirestore(appFirebase);
 
 router.post("/addLock", async (req, res) => {
   const auth = getAuth(appFirebase);
@@ -12,25 +12,26 @@ router.post("/addLock", async (req, res) => {
 
   // Check if the user is logged in
   if (user) {
-    // Reference to the user's node in the database
-    const userRef = ref(db, "users/" + user.uid);
+    // Reference to the user's document in the Firestore database
+    const userDoc = doc(db, "users", user.uid);
 
     // Store user information
-    set(userRef, {
+    setDoc(userDoc, {
       email: user.email,
-      name: "User Name",
+      name: "User Name New Test",
       other_info: "Other user-related information",
+      age: "20"
     })
-      .then(() => {
-        console.log("User information stored successfully!");
-        res.json({ message: "User information stored successfully!" });
-      })
-      .catch((error) => {
-        console.error("Error storing user information:", error);
-        res.status(500).json({ error: "Error storing user information" });
-      });
+    .then(() => {
+      console.log("User information stored successfully!");
+      res.json({ message: 'User information stored successfully!' });
+    })
+    .catch((error) => {
+      console.error("Error storing user information:", error);
+      res.status(500).json({ error: 'Error storing user information' });
+    });
   } else {
-    res.status(401).json({ error: "Not authenticated" });
+    res.status(401).json({ error: 'Not authenticated' });
   }
 });
 
